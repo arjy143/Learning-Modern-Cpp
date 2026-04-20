d964a0899c038ca1b4140d2b8a592416cb7d736e
Arjy143 <arjun_p_uk@yahoo.co.uk>
2026-04-20 15:04:24 +0100

    messing around

--- Operating Systems/Operating Systems 3 easy pieces/threading.c
+++ Operating Systems/Operating Systems 3 easy pieces/threading.c
@@ -5,8 +5,9 @@
 
 void* mythread(void* arg)
 {
-   printf("%s\n", (char*)arg); 
-   return NULL;
+    long long value = (long long)arg;
+    printf("%lld\n", value); 
+   return (void*)(value+1);
 }
 
 int main(int argc, char** argv)
@@ -14,17 +15,19 @@ int main(int argc, char** argv)
     pthread_t p1;
     pthread_t p2;
 
-    int rc;
+    long long return_value;
 
     printf("main: begin\n");
 
     //either of the 2 threads could complete first, depending on the scheduler.
-    pthread_create(&p1, NULL, mythread, "A");
-    pthread_create(&p2, NULL, mythread, "B");
+    pthread_create(&p1, NULL, mythread, (void*)100);
 
     //join takes in a pointer to the return value or null if void
-    pthread_join(p1, NULL);
-    pthread_join(p2, NULL);
+    pthread_join(p1, (void**)return_value);
+
+    pthread_create(&p2, NULL, mythread, (void*)return_value);
+
+    pthread_join(p2, (void**)return_value);
 
     printf("main: end\n");
 
