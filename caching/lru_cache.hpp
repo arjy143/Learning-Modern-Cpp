3da679f6013ab1c5a06b0e044c4863c056305585
Arjy143 <arjun_p_uk@yahoo.co.uk>
2026-06-17 09:22:27 +0100

    basic lru cache

---
+++ caching/lru_cache.hpp
@@ -0,0 +1,76 @@
+#include <unordered_map>
+#include <list>
+#include <optional>
+#include <iterator>
+
+
+template <typename K, typename V>
+class LRUCache
+{
+
+using Node = std::pair<K, V>;
+using ListIterator = typename std::list<Node>::iterator;
+
+private:
+    size_t capacity_;
+    std::unordered_map<K, ListIterator> lookup_table_;
+    std::list<Node> recency_list_;
+
+public:
+    LRUCache(size_t capacity) : capacity_(capacity) 
+    {}
+
+    std::optional<V> get(const K& key)
+    {
+        //if the key exists, then get the corresponding iterator, move it up the recency list, and return the associated value.
+        auto it = lookup_table_.find(key);
+        if (it != lookup_table_.end())
+        {
+            auto& iterator = it->second;
+
+            recency_list_.splice(recency_list_.end(), recency_list_, iterator);
+
+            return iterator->second;
+        }
+
+        return std::nullopt;
+    }
+
+    void put(K key, V value)
+    {
+        if (capacity_ == 0)
+        {
+            return;
+        }
+
+        //if key exists already, then overwrite it
+        auto it = lookup_table_.find(key);
+        if (it != lookup_table_.end())
+        {
+
+            //get old iterator, dereference it, overwrite the data, push it to the front.
+            auto& iterator = it->second;
+
+            iterator->second = std::move(value);
+
+            recency_list_.splice(recency_list_.end(), recency_list_, iterator);
+        }
+        else
+        {
+            //if key does not exist, then add it, and evict old data if needed.
+            if (recency_list_.size() >= capacity_)
+            {
+                auto iterator = recency_list_.begin();
+                
+                lookup_table_.erase(iterator->first);
+
+                recency_list_.pop_front();
+            }
+
+            auto new_iterator = recency_list_.emplace(recency_list_.end(), std::move(key), std::move(value));
+
+            lookup_table_.emplace(new_iterator->first, new_iterator); 
+        }
+    }
+
+};
