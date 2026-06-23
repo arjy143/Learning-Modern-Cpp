21b44ae9a635572a158e5fb7f6a068b9e2c17b86
Arjy143 <arjun_p_uk@yahoo.co.uk>
2026-06-23 15:10:26 +0100

    created 20% better performance dynamic array

--- data structures/dynamic_array.hpp
+++ data structures/dynamic_array.hpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <new>
 #include <utility>
+#include <cstring>
+#include <type_traits>
 
 template <typename T>
 class DynamicArray
@@ -12,8 +14,20 @@ private:
     size_t capacity_; 
     size_t item_count_;
 
+    void grow()
+    {
+        reserve(capacity_ ? capacity_ * 2 : 1);
+    }
+
+
 public:
-    DynamicArray(size_t capacity) : capacity_(capacity ? capacity : 1), item_count_(0)
+    DynamicArray() : capacity_(1), item_count_(0)
+    {
+        buffer_ = static_cast<T*>(malloc(sizeof(T) * capacity_));        
+    }
+
+    //explicit needed because otherwise the compiler would treat size_t as implicitly convertable to a DynamicArray
+    explicit DynamicArray(size_t capacity) : capacity_(capacity ? capacity : 1), item_count_(0)
     {
         buffer_ = static_cast<T*>(malloc(sizeof(T) * capacity_));        
     }
@@ -43,7 +57,7 @@ public:
         return *this;
     }
 
-    DynamicArray(DynamicArray&& other) noexcept : buffer_(other.buffer), capacity_(other.capacity_), item_count_(other.item_count_) 
+    DynamicArray(DynamicArray&& other) noexcept : buffer_(other.buffer_), capacity_(other.capacity_), item_count_(other.item_count_) 
     {
         other.buffer_ = nullptr;
         other.capacity_= 0;
@@ -63,33 +77,55 @@ public:
 
     void push_back(const T& item)
     {
+        emplace_back(item);
+    }
+
+    //forward referencing pack - keep track of lvalues and rvalues
+    template <typename... Args>
+    void emplace_back(Args&&... args)
+    {
         if (item_count_ >= capacity_)
         {
-            size_t new_capacity = capacity_ * 2;
+            grow();
+        }
 
-            
-            T* new_buffer = static_cast<T*>(malloc(sizeof(T) * new_capacity));
+        //perfect forwarding of all arguments into T's constructor, which removes the need for any temporary copy when constructing the item.
+        ::new (static_cast<void*>(buffer_ + item_count_)) T(std::forward<Args>(args)...);
+        ++item_count_;
+    }
 
+    void reserve(size_t new_capacity)
+    {
+        if (new_capacity <= capacity_)
+        {
+            return;
+        }
+
+        T* new_buffer = static_cast<T*>(malloc(sizeof(T) * new_capacity));
+
+        //small optimisation to do a single copy instead of multiple moves as long as the object only consists of its bytes.
+        //constexpr to allow branching at compile time
+        if constexpr (std::is_trivially_copyable_v<T>)
+        {
+            memcpy(new_buffer, buffer_, item_count_ * sizeof(T));
+        }
+        else
+        {
             for (size_t i = 0; i < item_count_; ++i)
             {
                 //move each item to the new buffer
-                ::new (static_cast<void*>(new_buffer + i)) T(std::move(buffer_[i]));
+                //move_if_noexcept just in case T's move constructor is not noexcept. If so, it will fall back to copying
+                ::new (static_cast<void*>(new_buffer + i)) T(std::move_if_noexcept(buffer_[i]));
 
                 //destruct the old item
                 buffer_[i].~T();
             }
-
-            free(buffer_);
-            buffer_ = new_buffer;
-            capacity_ = new_capacity;
         }
 
-        //placement copy construct
-        ::new (static_cast<void*>(buffer_ + item_count_)) T(item);
-        ++item_count_;
-
+        free(buffer_);
+        buffer_ = new_buffer;
+        capacity_ = new_capacity;
     }
-
 
     T& operator[](size_t index)
     {
@@ -101,5 +137,42 @@ public:
     const T& operator[](size_t index) const
     {
         return buffer_[index];
+    }
+
+    //mutable iterators
+    T* begin()
+    {
+        return buffer_;
+    }
+
+    T* end()
+    {
+        return buffer_ + item_count_;
+    }
+
+    //immutable iterators
+    const T* begin() const
+    {
+        return buffer_;
+    }
+
+    const T* end() const
+    {
+        return buffer_ + item_count_;
+    }
+    //helpers
+    [[nodiscard]] size_t size() const
+    {
+        return item_count_;
+    }
+
+    [[nodiscard]] size_t capacity() const
+    {
+        return capacity_;
+    }
+
+    [[nodiscard]] bool empty() const
+    {
+        return item_count_ == 0;
     }
 };
